64dcf3b0e1259f24c0aa0b1b70113f4eaf74342b
manny i. agbayani <serverless.yoda@gmail.com>
2020-05-22 08:34:40 +1200

    added bat code

---
+++ Contract/BatToken.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.6.3;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+
+contract BatToken is ERC20{
+    constructor() ERC20('BAT', 'Brave browser token') public {}
+}
