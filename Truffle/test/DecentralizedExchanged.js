0718f0aa190e987ebbf9f7aae03787df8e4c4f9d
manny i. agbayani <serverless.yoda@gmail.com>
2020-06-08 20:09:41 +1200

    Deploy The Dex smart contract

--- Truffle/test/DecentralizedExchanged.js
+++ Truffle/test/DecentralizedExchanged.js
@@ -2,10 +2,12 @@ const zrx = artifacts.require("ZrxToken.sol");
 const rep = artifacts.require("RepToken.sol");
 const dai = artifacts.require("DaiToken.sol");
 const bat = artifacts.require("BatToken.sol");
+const dex = artifacts.require("DecExchange.sol");
 
 contract('DecExchange', () => {
     let zrx,rep,dai,bat;
 
+    const [ZRX,REP,DAI,BAT] = ['ZRX','REP','DAI','BAT'].map(ticker => web3.utils.fromAscii(ticker));
     beforeEach(async() => {
         ([zrx,rep,dai,bat] = await Promise.all([
             zrx.new(),
@@ -13,5 +15,12 @@ contract('DecExchange', () => {
             dai.new(),
             bat.new()
         ]));
+        const Dex = await dex.new();
+        await Promise.all([
+             Dex.addTokenDTO(zrx.address,ZRZ),
+             Dex.addTokenDTO(rep.address,REP),
+             Dex.addTokenDTO(dai.address,DAI),
+             Dex.addTokenDTO(bat.address,BAT)
+        ]);
     })
 })
