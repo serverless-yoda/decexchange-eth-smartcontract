e587c2784351e9b7ffa132fc7902975ca579216a
manny i. agbayani <serverless.yoda@gmail.com>
2020-06-09 19:08:57 +1200

    add initial token balances

--- Truffle/test/DecentralizedExchanged.js
+++ Truffle/test/DecentralizedExchanged.js
@@ -4,10 +4,13 @@ const dai = artifacts.require("DaiToken.sol");
 const bat = artifacts.require("BatToken.sol");
 const dex = artifacts.require("DecExchange.sol");
 
-contract('DecExchange', () => {
+contract('DecExchange', (accounts) => {
     let zrx,rep,dai,bat;
 
     const [ZRX,REP,DAI,BAT] = ['ZRX','REP','DAI','BAT'].map(ticker => web3.utils.fromAscii(ticker));
+
+    const [trader1, trader2] = [accounts[1],accounts[2]];
+
     beforeEach(async() => {
         ([zrx,rep,dai,bat] = await Promise.all([
             zrx.new(),
@@ -17,10 +20,24 @@ contract('DecExchange', () => {
         ]));
         const Dex = await dex.new();
         await Promise.all([
-             Dex.addTokenDTO(zrx.address,ZRZ),
+             Dex.addTokenDTO(zrx.address,ZRX),
              Dex.addTokenDTO(rep.address,REP),
              Dex.addTokenDTO(dai.address,DAI),
              Dex.addTokenDTO(bat.address,BAT)
         ]);
+
+        const amount = web3.utils.toWei('1000');
+        const seedTokenBalance = async(token,trader) => {
+            await token.faucet(trader, amount);
+            await token.approved(Dex.address,amount, {from: trader})            
+        };
+
+        await Promise.all(
+            [zrx,rep,dai,bat].map(token => seedTokenBalance(token,trader1))
+        );
+
+        await Promise.all(
+            [zrx,rep,dai,bat].map(token => seedTokenBalance(token,trader2))
+        );
     })
 })
